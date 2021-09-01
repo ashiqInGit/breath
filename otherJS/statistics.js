33f91244b8f10483791e79257eb83587c92789d3
ashiqInGit <ashiqkhanj001@gmail.com>
2021-09-01 18:23:41 +0530

    Added chat Bot but not quite working

--- otherJS/statistics.js
+++ otherJS/statistics.js
@@ -4,7 +4,7 @@ var alldata=[
         data: [0.33, 0.65, 3.45, 4.84, 2.04, 6.64],
         borderColor: '#00B7A8',
         borderWidth:2,
-        tension:0.17,
+        tension:0,
 
     },
     {
@@ -12,7 +12,7 @@ var alldata=[
         data: [0.26, 1.09, 1.66, 4.12, 1.90, 4.65],
         borderColor: 'blue',
         borderWidth:2,
-        tension:0.17,
+        tension:0,
 
     },
     {
@@ -20,7 +20,7 @@ var alldata=[
         data: [0.25, 0.60, 0.94, 3.44, 1.40, 3.76],
         borderColor: '#1C2B2D',
         borderWidth:2,
-        tension:0.17,
+        tension:0,
     }
 ];
 
@@ -28,8 +28,8 @@ var alldata=[
 // +++++++++++++++++++++++++ ADDING CHART ++++++++++++++++++++++++++++++++++++
 
 
-var ctx = document.getElementById('myChart').getContext('2d');
-var myChart = new Chart(ctx, {
+var canvas = document.getElementById('myChart').getContext('2d');
+var myChart = new Chart(canvas, {
     type: 'line',
     data: {
         labels: [
@@ -107,6 +107,13 @@ function randomColorPicker(){
 
 
 function updateChart(SelectedcountryName){
+
+
+    for(let i=0;i<alldata.length;i++){
+        if(SelectedcountryName===alldata[i]["label"]){
+            return;
+        }
+    }
   
 
     for(let i=0;i<data.length;i++){
@@ -115,7 +122,7 @@ function updateChart(SelectedcountryName){
 
             data[i]["borderColor"]=`#${randomColorPicker()}`;
             data[i].borderWidth= 2;
-            data[i].tension=0.17;
+            data[i].tension=0;
             data[i].backgroundColor='transparent';
 
             alldata.push(data[i]);
